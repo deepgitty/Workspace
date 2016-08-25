139bb4d7e21b1728d41a5e6c4e5a43c33f86fb08
bolyrics <bolyrics@70c37e83-f50c-44ee-bfde-f6a21ebed1b0>
2016-08-25 16:01:41 +0000

    Project structure

---
+++ trunk/bol/src/main/java/com/bol/config/RootConfig.java
@@ -0,0 +1,13 @@
+package com.bol.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan(basePackages = { "com.bol" }, excludeFilters = { @Filter(type = FilterType.ANNOTATION, value = EnableWebMvc.class) })
+public class RootConfig {
+
+}
